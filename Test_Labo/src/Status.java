8dcd52c5aa6cc237d31bf16b6de0ecede6a07e5e
x13g008 <x13g008@PC205N007.chiba-fjb.ac.jp>
2014-07-11 19:04:41 +0900

    未完成ですが提出します。

---
+++ Test_Labo/src/Status.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+
+public class Status {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Scanner s = new Scanner(System.in);
+		int hp;
+		int exp;
+		int money;
+		String name;
+		
+		
+        System.out.println("HPを入力");
+        hp = s.nextInt();
+        System.out.println("EXPを入力");
+        exp = s.nextInt(); 
+        System.out.println("MONEYを入力");
+        money = s.nextInt();
+        System.out.println("名前を入力");
+        String a = s.next();
+		}
+	}
